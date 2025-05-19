a394c75c293e684c0679ec0a8a827e0324de79ca
tuankiet2004444 <nguyenkiet01072004@gmail.com>
2025-05-19 11:07:54 +0700

    Add files via upload

---
+++ WEBBANHANG/app/views/shares/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Quản lý sản phẩm</title> 
+    <link 
+href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
+rel="stylesheet"> 
+</head> 
+<body> 
+    <nav class="navbar navbar-expand-lg navbar-light bg-light"> 
+        <a class="navbar-brand" href="#">Quản lý sản phẩm</a> 
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data
+target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle 
+navigation"> 
+            <span class="navbar-toggler-icon"></span> 
+        </button> 
+        <div class="collapse navbar-collapse" id="navbarNav"> 
+            <ul class="navbar-nav"> 
+                <li class="nav-item"> 
+                    <a class="nav-link" href="/WEBBANHANG/Product/">Danh sách sản 
+phẩm</a> 
+                </li> 
+                <li class="nav-item"> 
+                    <a class="nav-link" href="/WEBBANHANG/Product/add">Thêm sản 
+phẩm</a> 
+                </li> 
+            </ul> 
+        </div> 
+    </nav> 
+    <div class="container mt-4">
